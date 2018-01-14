f69c0c0b65d1169dabccb850c0f15deb4cd7d797
Scott Haney <emailalmondbranch@gmail.com>
2018-01-14 21:59:20 +0000

    Add a test to make sure that files are ignored if they don't match the expected extension

--- spec/file_finder_spec.rb
+++ spec/file_finder_spec.rb
@@ -12,15 +12,32 @@ RSpec.describe AlmondBackup::FileFinder do
       let(:base_directory) { '/base_test_dir/' }
 
       context 'given a .jpg file located directly in the root directory' do
-        let(:jpg_file_name) { 'test.jpg' }
+        let(:jpg_rel_path) { 'test.jpg' }
+        let(:jpg_path) { File.join(base_directory, jpg_rel_path) }
+
+        before :example do
+          FileUtils.mkdir_p(File.dirname(jpg_path))
+          FileUtils.touch jpg_path
+        end
             
         it 'finds the .jpg file located directly in the root directory' do
-          file_path = File.join(base_directory, jpg_file_name)
-          FileUtils.mkdir_p(File.dirname(file_path))
-          FileUtils.touch file_path
-
           jpgs = file_finder.find(base_directory, '.jpg')
-          expect(jpgs).to eq([file_path])
+          expect(jpgs).to eq([jpg_path])
+        end
+
+        context 'given a .txt file located directly in the root directory' do
+          let(:txt_rel_path) { 'test.txt' }
+          let(:txt_path) { File.join(base_directory, txt_rel_path) }
+
+          before :example do
+            FileUtils.mkdir_p(File.dirname(txt_path))
+            FileUtils.touch txt_path
+          end
+
+          it 'finds only the .jpg file located directly in the root directory' do
+            jpgs = file_finder.find(base_directory, '.jpg')
+            expect(jpgs).to eq([jpg_path])
+          end
         end
       end
     end
