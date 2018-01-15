71170ce498fa94fbded362a716e18d998eb1c4f3
Scott Haney <emailalmondbranch@gmail.com>
2018-01-15 16:38:01 +0000

    Refactor out code to create a directory in file_finder_spec from the code to create a file

--- spec/file_finder_spec.rb
+++ spec/file_finder_spec.rb
@@ -2,12 +2,19 @@ require 'fakefs/spec_helpers'
 require 'fileutils'
 require 'almond_backup/file_finder'
 
-def create_file(rel_file_path)
-  var_name = "#{File.basename(rel_file_path).gsub('.','_')}_path"
-  let(var_name) { File.join(base_directory, rel_file_path) }
+def create_directory(directory, var_name)
+  let(var_name) { directory }
 
   before :example do
-    FileUtils.mkdir_p(File.dirname(eval(var_name)))
+    FileUtils.mkdir_p(eval(var_name.to_s))
+  end
+end
+
+def create_file(rel_file_path)
+  var_name = "#{File.basename(rel_file_path).gsub('.','_')}_path"
+  let(var_name.to_sym) { File.join(base_directory, rel_file_path) }
+
+  before :example do
     FileUtils.touch eval(var_name)
   end
 end
@@ -18,7 +25,7 @@ RSpec.describe AlmondBackup::FileFinder do
     include FakeFS::SpecHelpers
 
     context 'given a root directory to search' do
-      let(:base_directory) { '/base_test_dir/' }
+      create_directory '/base_test_dir/', :base_directory
 
       context 'given a .jpg file located directly in the root directory' do
         create_file 'test.jpg'
