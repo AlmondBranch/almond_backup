2026e6f1f9332071153cbbd26a68ef5c1c9717b6
Scott Haney <emailalmondbranch@gmail.com>
2018-01-19 01:58:51 +0000

    Dry out the single matching file checks in file_finder_spec.rb

--- spec/file_finder_spec.rb
+++ spec/file_finder_spec.rb
@@ -20,50 +20,39 @@ RSpec.describe AlmondBackup::FileFinder do
   describe "#find" do
     include FakeFS::SpecHelpers
 
+    shared_examples 'finds a single matching file' do |file|
+      create_file file
+
+      it 'finds that file' do
+        files = subject.find(base_directory, file_extensions)
+        expect(files).to eq([File.join(base_directory, file)])
+      end
+    end
+
     context 'when searching for a single file extension' do
       create_directory '/base'
 
       context 'that is lower case' do
         let(:file_extensions) { '.jpg' }
 
-        context 'and a file with an identical extension exists' do
-          create_file 'test.jpg'
-
-          it 'finds that file' do
-            files = subject.find(base_directory, file_extensions)
-            expect(files).to eq(['/base/test.jpg'])
-          end
+        context 'and a single file with an identical extension exists' do
+          include_examples 'finds a single matching file', 'test.jpg'
         end
         
-        context 'and a file with an extension differing only by case exists' do
-          create_file 'test.JPG'
-
-          it 'finds that file' do
-            files = subject.find(base_directory, file_extensions)
-            expect(files).to eq(['/base/test.JPG'])
-          end
+        context 'and a single file with an extension differing only by case exists' do
+          include_examples 'finds a single matching file', 'test.JPG'
         end
       end
 
       context 'that is upper case' do
         let(:file_extensions) { '.JPG' }
 
-        context 'and a file with an identical extension exists' do
-          create_file 'test.JPG'
-
-          it 'finds that file' do
-            files = subject.find(base_directory, file_extensions)
-            expect(files).to eq(['/base/test.JPG'])
-          end
+        context 'and a single file with an identical extension exists' do
+          include_examples 'finds a single matching file', 'test.JPG'
         end
         
-        context 'and a file with an extension differing only by case exists' do
-          create_file 'test.jpg'
-
-          it 'finds that file' do
-            files = subject.find(base_directory, file_extensions)
-            expect(files).to eq(['/base/test.jpg'])
-          end
+        context 'and a single file with an extension differing only by case exists' do
+          include_examples 'finds a single matching file', 'test.jpg'
         end
       end
     end
