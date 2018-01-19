4ce9ee15f2876359f197e93c1ccc9607f9a17f57
Scott Haney <emailalmondbranch@gmail.com>
2018-01-19 01:24:07 +0000

    Clean up the tests in file_finder_spec.rb

--- spec/file_finder_spec.rb
+++ spec/file_finder_spec.rb
@@ -11,56 +11,61 @@ def create_directory(full_dir_path)
 end
 
 def create_file(file_name)
-  var_name = "#{file_name.gsub('.','_')}_path"
-  let(var_name.to_sym) { File.join(base_directory, file_name) }
-
   before :example do
-    FileUtils.touch eval(var_name)
+    FileUtils.touch File.join(base_directory, file_name)
   end
 end
 
 RSpec.describe AlmondBackup::FileFinder do
-
-  shared_examples_for 'search for files' do |extension|
-create_directory '/base_test_dir/'
-
-      context 'having a file test.jpg' do
-        create_file 'test.jpg'
-            
-        it 'finds test.jpg' do
-          jpgs = subject.find(base_directory, extension)
-          expect(jpgs).to eq(['/base_test_dir/test.jpg'])
-        end
-
-        context 'and having a file test.txt' do
-          create_file 'test.txt'
-
-          it 'finds test.jpg and not test.txt' do
-            jpgs = subject.find(base_directory, extension)
-            expect(jpgs).to eq(['/base_test_dir/test.jpg'])
-          end
-        end
-      end
-      
-      context 'having a file test.JPG' do
-        create_file 'test.JPG'
-
-        it 'finds test.JPG' do
-          jpgs = subject.find(base_directory, extension)
-          expect(jpgs).to eq(['/base_test_dir/test.JPG'])
-        end
-      end
-  end
-
   describe "#find" do
     include FakeFS::SpecHelpers
 
-    context 'given a root directory to search for .jpg files' do
-      it_behaves_like 'search for files', '.jpg'
-    end
+    context 'when searching for a single file extension' do
+      create_directory '/base'
 
-    context 'given a root directory to search for .JPG files' do
-      it_behaves_like 'search for files', '.JPG'
+      context 'that is lower case' do
+        let(:file_extensions) { '.jpg' }
+
+        context 'and a file with an identical extension exists' do
+          create_file 'test.jpg'
+
+          it 'finds that file' do
+            files = subject.find(base_directory, file_extensions)
+            expect(files).to eq(['/base/test.jpg'])
+          end
+        end
+        
+        context 'and a file with an extension differing only by case exists' do
+          create_file 'test.JPG'
+
+          it 'finds that file' do
+            files = subject.find(base_directory, file_extensions)
+            expect(files).to eq(['/base/test.JPG'])
+          end
+        end
+      end
+
+      context 'that is upper case' do
+        let(:file_extensions) { '.JPG' }
+
+        context 'and a file with an identical extension exists' do
+          create_file 'test.JPG'
+
+          it 'finds that file' do
+            files = subject.find(base_directory, file_extensions)
+            expect(files).to eq(['/base/test.JPG'])
+          end
+        end
+        
+        context 'and a file with an extension differing only by case exists' do
+          create_file 'test.jpg'
+
+          it 'finds that file' do
+            files = subject.find(base_directory, file_extensions)
+            expect(files).to eq(['/base/test.jpg'])
+          end
+        end
+      end
     end
   end
 end
