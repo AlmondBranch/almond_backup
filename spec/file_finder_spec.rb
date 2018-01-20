acd4ba63cba05a679074a6476e3a0a67a8d46286
Scott Haney <emailalmondbranch@gmail.com>
2018-01-20 17:26:09 +0000

    Add some more tests to file_finder_spec.rb and reorganize it to make it more clear

--- spec/file_finder_spec.rb
+++ spec/file_finder_spec.rb
@@ -9,72 +9,89 @@ RSpec.describe AlmondBackup::FileFinder do
   describe "#find" do
     include FakeFS::SpecHelpers
 
-    context 'when searching for a single file extension' do
-      context 'that is lower case' do
-        let(:file_extensions) { '.jpg' }
+    context 'when a matching file exists in a subdirectory' do
+      create_file '/base/sub/test.jpg'
 
-        context 'and a single file with an identical extension exists' do
-          create_file '/base/test.jpg'
+      it 'finds the file' do
+        files = subject.find('/base', '.jpg')
+        expect(files).to eq(['/base/sub/test.jpg'])
+      end
+    end
 
-          it 'finds the file' do
-            files = subject.find('/base', file_extensions)
-            expect(files).to eq(['/base/test.jpg'])
-          end
-        end
-        
-        context 'and a single file with an extension differing only by case exists' do
-          create_file '/base/test.JPG'
+    context 'when a file with an extension that is not being searched for exists' do
+      create_file '/base/test.png'
 
-          it 'finds the file' do
-            files = subject.find('/base', file_extensions)
-            expect(files).to eq(['/base/test.JPG'])
-          end
+      it 'does not find the file' do
+        files = subject.find('/base', '.jpg')
+        expect(files).to be_empty
+      end
+    end
+
+    context 'when an upper case file extension is being searched for' do
+      let(:file_extensions) { '.JPG' }
+
+      context 'and a file with an identical extension exists' do
+        create_file '/base/test.JPG'
+
+        it 'finds the file' do
+          files = subject.find('/base', file_extensions)
+          expect(files).to eq(['/base/test.JPG'])
         end
       end
 
-      context 'that is upper case' do
-        let(:file_extensions) { '.JPG' }
+      context 'and a file with an extension differing only by case exists' do
+        create_file '/base/test.jpg'
 
-        context 'and a single file with an identical extension exists' do
-          create_file '/base/test.JPG'
-
-          it 'finds the file' do
-            files = subject.find('/base', file_extensions)
-            expect(files).to eq(['/base/test.JPG'])
-          end
+        it 'finds the file' do
+          files = subject.find('/base', file_extensions)
+          expect(files).to eq(['/base/test.jpg'])
         end
-        
-        context 'and a single file with an extension differing only by case exists' do
-          create_file '/base/test.jpg'
+      end
+    end
 
-          it 'finds the file' do
-            files = subject.find('/base', file_extensions)
-            expect(files).to eq(['/base/test.jpg'])
-          end
+    context 'when a lower case file extension is being searched for' do
+      let(:file_extensions) { '.jpg' }
+
+      context 'and a file with an identical extension exists' do
+        create_file '/base/test.jpg'
+
+        it 'finds the file' do
+          files = subject.find('/base', file_extensions)
+          expect(files).to eq(['/base/test.jpg'])
+        end
+      end
+
+      context 'and a file with an extension differing only by case exists' do
+        create_file '/base/test.JPG'
+
+        it 'finds the file' do
+          files = subject.find('/base', file_extensions)
+          expect(files).to eq(['/base/test.JPG'])
         end
       end
     end
 
     context 'when searching for multiple file extensions' do
-      let(:file_extensions) { ['.txt', '.JPG'] }
-
-      context 'and two files with identical extensions exist' do
-        create_file '/base/test.txt'
-        create_file '/base/test.JPG'
+      context 'and a single matching file exists for each extension' do
+        create_file '/base/sub/test.jpg'
+        create_file '/base/sub/sub/test.TXT'
+        create_file '/base/test.png'
 
         it 'finds the files' do
-          files = subject.find('/base', *file_extensions)
-          expect(files).to contain_exactly('/base/test.txt', '/base/test.JPG')
+          files = subject.find('/base', '.jpg', '.txt')
+          expect(files).to contain_exactly('/base/sub/test.jpg', '/base/sub/sub/test.TXT')
         end
       end
+    end
 
-      context 'and two files with extensions differing only by case exist' do
-        create_file '/base/test.TXT'
-        create_file '/base/test.jpg'
+    context 'when searching for a single file extension' do
+      context 'and a single matching file exists' do
+        create_file '/base/sub/test.txt'
+        create_file '/base/sub/test.jpg'
 
-        it 'finds the files' do
-          files = subject.find('/base', *file_extensions)
-          expect(files).to contain_exactly('/base/test.TXT', '/base/test.jpg')
+        it 'finds the file' do
+          files = subject.find('/base', '.JPG')
+          expect(files).to eq(['/base/sub/test.jpg'])
         end
       end
     end
