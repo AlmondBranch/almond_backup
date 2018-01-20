ee413a5ada2bfe34ca4640898d64be6d6abe2c78
Scott Haney <emailalmondbranch@gmail.com>
2018-01-20 17:05:13 +0000

    Add tests for searching for multiple file extensions

--- spec/file_finder_spec.rb
+++ spec/file_finder_spec.rb
@@ -54,5 +54,29 @@ RSpec.describe AlmondBackup::FileFinder do
         end
       end
     end
+
+    context 'when searching for multiple file extensions' do
+      let(:file_extensions) { ['.txt', '.JPG'] }
+
+      context 'and two files with identical extensions exist' do
+        create_file '/base/test.txt'
+        create_file '/base/test.JPG'
+
+        it 'finds the files' do
+          files = subject.find('/base', *file_extensions)
+          expect(files).to contain_exactly('/base/test.txt', '/base/test.JPG')
+        end
+      end
+
+      context 'and two files with extensions differing only by case exist' do
+        create_file '/base/test.TXT'
+        create_file '/base/test.jpg'
+
+        it 'finds the files' do
+          files = subject.find('/base', *file_extensions)
+          expect(files).to contain_exactly('/base/test.TXT', '/base/test.jpg')
+        end
+      end
+    end
   end
 end
