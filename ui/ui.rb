2fb8322e6c533c7665f407f09e9ee85ce41e54f5
Scott Haney <emailalmondbranch@gmail.com>
2018-07-18 23:50:04 +0000

    Add .mov file extensions to the list of files to backup in the ui

--- ui/ui.rb
+++ ui/ui.rb
@@ -15,7 +15,7 @@ Shoes.app width: 320, height: 420 do
     stack margin: 10 do
       button 'Run Backup' do
         runner = AlmondBackup::BackupRunner.new(AlmondBackup::FileFinder.new)
-        runner.run_backup(@source.text, @destination.text, '.m4v', '.m4a', '.jpg')
+        runner.run_backup(@source.text, @destination.text, '.m4v', '.m4a', '.jpg', '.mov')
       end
     end
   end
