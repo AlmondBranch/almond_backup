c2cedcc28e759b754364cce82ad36268cb087dba
Scott Haney <emailalmondbranch@gmail.com>
2018-07-19 01:43:20 +0000

    Add an exception message box at the bottom of the ui to easily see error messages

--- ui/ui.rb
+++ ui/ui.rb
@@ -1,6 +1,6 @@
 require 'almond_backup'
 
-Shoes.app width: 320, height: 420 do
+Shoes.app width: 320, height: 620 do
   stack margin: 40 do
     stack margin: 10 do
       para 'Source Folder'
@@ -14,9 +14,19 @@ Shoes.app width: 320, height: 420 do
 
     stack margin: 10 do
       button 'Run Backup' do
+        begin
         runner = AlmondBackup::BackupRunner.new(AlmondBackup::FileFinder.new)
         runner.run_backup(@source.text, @destination.text, '.m4v', '.m4a', '.jpg', '.mov')
+        rescue Exception => e
+          @details.text = e.to_s
+        end
       end
+    end
+
+    stack margin: 10 do
+      para 'Exception Details'
+      @details = edit_box
+      @details.height = 300
     end
   end
 end
